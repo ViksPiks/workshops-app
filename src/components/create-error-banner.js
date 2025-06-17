d2204d9da4621f6a6d7af48be7b723489072c242
viks <victoria.miledina@gmail.com>
2025-06-17 20:47:28 +0200

    style error handler

--- src/components/create-error-banner.js
+++ src/components/create-error-banner.js
@@ -1,5 +1,7 @@
 export const createErrorBanner = (error) => {
   const paragraph = document.createElement("p");
+  paragraph.classList.add("alert", "alert-danger");
+  paragraph.setAttribute("role", "alert");
   paragraph.textContent = `Unable to load workshops due to error: "${error.message}"`;
   return paragraph;
 };
